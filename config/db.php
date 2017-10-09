295678b8aae285d08a2f0da8568f40f4d06865f9
Alexandr <p.alexandrsergeevich@gmail.com>
2017-10-09 20:47:21 +0300

    connect to db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii_application',
-    'username' => 'root',
-    'password' => '124578alex',
+    'dsn' => 'mysql:host=localhost;dbname=u713005188_store',
+    'username' => 'u713005188_moda',
+    'password' => 'Qqscgyop1835',
     'charset' => 'utf8',
 ];
